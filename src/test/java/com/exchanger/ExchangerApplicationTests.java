01bb3e89fbe1523f5b18a28f5513c3e1dddd44e8
Mehmet Genc <mehmetgencv@gmail.com>
2025-05-19 23:31:19 +0300

    initialize Spring Boot project with base dependencies

---
+++ src/test/java/com/exchanger/ExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
