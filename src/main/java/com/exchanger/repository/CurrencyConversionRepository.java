147526e268add956a635df3c75992cebcbfd2114
Mehmet Genc <mehmetgencv@gmail.com>
2025-05-23 00:30:55 +0300

    add findAllByTransactionDateBetween query

--- src/main/java/com/exchanger/repository/CurrencyConversionRepository.java
+++ src/main/java/com/exchanger/repository/CurrencyConversionRepository.java
@@ -1,10 +1,13 @@
 package com.exchanger.repository;
 
 import com.exchanger.entity.CurrencyConversion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface CurrencyConversionRepository extends JpaRepository<CurrencyConversion, UUID> {
-
+    Page<CurrencyConversion> findAllByTransactionDateBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
 }
