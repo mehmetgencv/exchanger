0d92119bae1b3d68f2945c87cb3df40f2f62b1e3
Mehmet Genc <mehmetgencv@gmail.com>
2025-05-22 19:16:49 +0300

    add getExchangeRate methods

--- src/main/java/com/exchanger/service/CurrencyConversionService.java
+++ src/main/java/com/exchanger/service/CurrencyConversionService.java
@@ -1,4 +1,10 @@
 package com.exchanger.service;
 
+import com.exchanger.dto.requests.ExchangeRateRequest;
+import com.exchanger.dto.responses.ExchangeRateResponse;
+import com.exchanger.dto.responses.SingleExchangeRateResponse;
+
 public interface  CurrencyConversionService {
+    SingleExchangeRateResponse getSingleExchangeRate(ExchangeRateRequest request);
+    ExchangeRateResponse getExchangeRates(ExchangeRateRequest request);
 }
