2313e8fb8f8df3ae60d722a4df1889b666904ada
Mehmet Genc <mehmetgencv@gmail.com>
2025-05-22 19:18:19 +0300

    add getExchangeRate endpoint

--- src/main/java/com/exchanger/controller/CurrencyConversionController.java
+++ src/main/java/com/exchanger/controller/CurrencyConversionController.java
@@ -1,11 +1,35 @@
 package com.exchanger.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.exchanger.dto.requests.ExchangeRateRequest;
+import com.exchanger.dto.responses.ExchangeRateResponse;
+import com.exchanger.dto.responses.SingleExchangeRateResponse;
+import com.exchanger.service.CurrencyConversionService;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/conversion")
 public class CurrencyConversionController {
 
+    private final CurrencyConversionService currencyConversionService;
+
+    public CurrencyConversionController(CurrencyConversionService currencyConversionService) {
+        this.currencyConversionService = currencyConversionService;
+    }
+
+    @GetMapping("/exchange-rate")
+    public ResponseEntity<SingleExchangeRateResponse> getExchangeRate(
+            @RequestParam @NotBlank String source,
+            @RequestParam @NotBlank String target
+    ) {
+
+        ExchangeRateRequest request = new ExchangeRateRequest(source.toUpperCase(), List.of(target.toUpperCase()) );
+        SingleExchangeRateResponse response = currencyConversionService.getSingleExchangeRate(request);
+
+        return ResponseEntity.ok(response);
+    }
 }
