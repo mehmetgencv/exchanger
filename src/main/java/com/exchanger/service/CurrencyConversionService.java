56c7972a64551a4a8b7152441590f6e0b28e732d
Mehmet Genc <mehmetgencv@gmail.com>
2025-05-23 00:31:17 +0300

    add getHistory Method

--- src/main/java/com/exchanger/service/CurrencyConversionService.java
+++ src/main/java/com/exchanger/service/CurrencyConversionService.java
@@ -1,13 +1,19 @@
 package com.exchanger.service;
 
+import com.exchanger.dto.requests.CurrencyConversionHistoryRequest;
 import com.exchanger.dto.requests.CurrencyConversionRequest;
 import com.exchanger.dto.requests.ExchangeRateRequest;
+import com.exchanger.dto.responses.CurrencyConversionHistoryResponse;
 import com.exchanger.dto.responses.CurrencyConversionResponse;
 import com.exchanger.dto.responses.ExchangeRateResponse;
 import com.exchanger.dto.responses.SingleExchangeRateResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 public interface  CurrencyConversionService {
     SingleExchangeRateResponse getSingleExchangeRate(ExchangeRateRequest request);
     ExchangeRateResponse getExchangeRates(ExchangeRateRequest request);
     CurrencyConversionResponse convert(CurrencyConversionRequest request);
+    Page<CurrencyConversionHistoryResponse> getHistory(CurrencyConversionHistoryRequest request, Pageable pageable);
 }
